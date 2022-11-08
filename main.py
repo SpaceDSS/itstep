174cfb93e9a0a04cc3fc2acb0ec7cf65b0fceed9
Lenovo <dkoshik2008@gmail.com>
2022-11-08 22:01:08 +0200

    Jurnal commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello World')
+ja='10'
+Jenya='11'
+dima='8'
+
+jurnalDanil=['Matematika {}'.format(ja)]
+jurnalJenya=['english {}'.format(Jenya)]
+jurnalDima=['Biology {}'.format(dima)]
+print(jurnalDima)
+print(jurnalJenya)
+print(jurnalDanil)
